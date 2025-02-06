20aeecdf5885a2a310f9e0b7c1c64684135ec735
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-02-06 21:38:39 +0100

    💄 Add necessary files to the generated plugin registration scripts for Linux, macOS, and Windows.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cryptography_flutter_plus
+import desktop_updater
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
+  DesktopUpdaterPlugin.register(with: registry.registrar(forPlugin: "DesktopUpdaterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
